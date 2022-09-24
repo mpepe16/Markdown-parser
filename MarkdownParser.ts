b0f0bbdf23164cf8b834e7fd14a1f3983855381f
mpepe16 <mpepe66@gmail.com>
2022-09-24 11:01:45 +0200

    Add: Markdown class in order to wire together the parsing flow.

--- MarkdownParser.ts
+++ MarkdownParser.ts
@@ -242,6 +242,22 @@ class HtmlHandler {
  }
 }
 
+    //This class is our last step to wire up the app. 
+    //The ToHtml method take a single string, call the MarkDownDocument, COR factory first class(Header1)
+    // Split the parameter and start to parse 
+class Markdown {
+    public ToHtml(text : string) : string {
+    let document : IMarkdownDocument = new MarkdownDocument();
+    let header1 : Header1ChainHandler = new ChainOfResponsibilityFactory().Build(document);
+    let lines : string[] = text.split(`\n`);
+    for (let index = 0; index < lines.length; index++) {
+    let parseElement : ParseElement = new ParseElement();
+    parseElement.CurrentLine = lines[index];
+    header1.HandleRequest(parseElement);
+    }
+    return document.Get();
+    }
+   }
 
        
     
